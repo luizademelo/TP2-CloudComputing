ed1f289941f4a5a772be654e49f217ae1d3b3c6b
luizademelo <ludemelo5452@gmail.com>
2024-12-26 17:24:05 -0300

    adding client frontend

--- app/app.py
+++ app/app.py
@@ -1,22 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # from recomender import compute_rules
 import pickle
-from flask import jsonify
+from recomender import compute_rules
 
 @app.route('/')
 def home():
    return "hello world!"
 
-# @app.route("/api/recommend", methods=["POST"])
-# def recommend(): 
-#     compute_rules() 
-#     app.model = pickle.load(open("rules.pickle", "rb"))
+@app.route("/api/recommend", methods=["POST"])
+def recommend(): 
+    data = request.json
+    compute_rules() 
+    app.model = pickle.load(open("rules.pickle", "rb"))
 
 
-#     recommendations = app.model['consequents'].tolist() 
-#     return jsonify({"songs": recommendations})
+    recommendations = app.model['consequents'].tolist() 
+    return jsonify({"songs": recommendations})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
