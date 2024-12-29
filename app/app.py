b5e3f9d8ed736d0bc2aa8f7a628a8fdc5992ff2f
luizademelo <ludemelo5452@gmail.com>
2024-12-29 10:44:46 -0300

    adding frontend

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from datetime import datetime
 
 app = Flask(__name__)
@@ -9,7 +9,7 @@ from model import compute_rules
 
 @app.route('/')
 def home():
-   return "hello world!"
+   return render_template("index.html")
 
 @app.route("/api/recommend", methods=["POST"])
 def recommend(): 
